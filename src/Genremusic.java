bf24b314cc406f8a9cd55a39421d4c33271343eb
Ljoohyeok <fgd8274@gmail.com>
2022-04-25 20:18:53 +0900

    5번째 과제입니다.

---
+++ src/Genremusic.java
@@ -0,0 +1,22 @@
+
+
+public class Genremusic extends PlayMusics {
+	
+	String[] musicgenre;
+	
+	public void setGenre(String[] musicgenre) {
+		this.musicgenre = musicgenre;
+	}
+	
+	public String[] getGenre() {
+		return this.musicgenre;
+	}
+	
+	public void PlaymusicData() {
+		super.PlaymusicData();
+		for(int i = 0;i <= 2;i++) {
+			System.out.println(musicname[i]);
+			System.out.println(musicname[i] + " " + "(" + musicgenre[i] + ")");
+		}
+	}
+}
---
+++ src/PlayMusics.java
@@ -0,0 +1,22 @@
+
+
+public class PlayMusics {
+	
+	String[] musicname;
+	
+	public void PlayMusic() {
+		
+	}
+
+	public void PlaymusicData() {
+		
+	}
+	
+	public void setter(String[] musicname) {
+		this.musicname = musicname;
+	}
+	
+	public String[] getter() {
+		return this.musicname;
+	}
+}
